dc7596ff4677f994287267e3ffeade0880331441
Codecraft <paul.massmann@web.de>
2025-10-30 18:14:14 +0100

    weather api

---
+++ script.js
@@ -0,0 +1,45 @@
+const apiKey = "2cc73287bf1471236962b65a5be6a3d0";
+
+document.getElementById("SearchButton").addEventListener("click", async function () {
+    const rawInput = document.getElementById("SearchInput").value.trim();
+    const city = encodeURIComponent(rawInput);
+
+    const emojiEl = document.querySelector(".today");
+    const tempEl = document.querySelector(".temperature");
+
+    if (!city) return;
+
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+        );
+        const data = await response.json();
+
+        if (data.cod !== 200) {
+            console.log("Fehlerantwort:", data); // zeigt dir genaue Ursache
+            emojiEl.textContent = "❓";
+            tempEl.textContent = `Fehler: ${data.message}`;
+            return;
+        }
+
+        const temp = Math.round(data.main.temp);
+        //let emoji = "";
+
+        if (temp >= 20) {
+            emoji = "☀️";
+        } else if (temp >= 12) {
+            emoji = "⛅";
+        } else if (temp >= 6) {
+            emoji = "☁️";
+        } else {
+            emoji = "❄️";
+        }
+
+        emojiEl.textContent = emoji;
+        tempEl.textContent = `${temp}°C in ${data.name}`;
+    } catch (error) {
+        emojiEl.textContent = "⚠️";
+        tempEl.textContent = "Fehler beim Abrufen";
+        console.error(error);
+    }
+});
